f7faedb18aa05e583d493c7b1e0f1e6566b3a356
John Yenter-Briars <jyenterbriars@gmail.com>
2022-03-22 14:28:09 -0500

    background slider project

---
+++ background_slider/script.js
@@ -0,0 +1,31 @@
+const body = document.body;
+const slides = document.querySelectorAll('.slide');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+
+let activeSlide = 0;
+
+rightBtn.addEventListener('click', () => {
+    activeSlide = activeSlide > slides.length ? 0 : activeSlide + 1;
+    setBgToBody();
+    setActiveSlide();
+});
+
+leftBtn.addEventListener('click', () => {
+    activeSlide = activeSlide < 1 ? slides.length - 1 : activeSlide - 1;
+    setBgToBody();
+    setActiveSlide();
+});
+
+setBgToBody();
+
+function setBgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+    slides.forEach(slide => slide.classList.remove('active'));
+
+    slides[activeSlide].classList.add('active');
+}
+
